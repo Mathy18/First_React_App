6bfd0968717bfd9cee9fdd958a125da0db01a238
unknown <indu.raja18@gmail.com>
2020-04-23 23:11:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar"></img>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Indu
+          </a>
+          <div className="metadata"></div>
+          Today 6:00 pm
+          <div className="text">Nice blog post</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
