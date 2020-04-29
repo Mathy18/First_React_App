5abe2ab6616e36c7c79e39d40801bea3010b5cd2
unknown <indu.raja18@gmail.com>
2020-04-29 17:03:01 +0530

    updated

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
 const ApprovalCard = (props) => {
-console.log(props.children);
+console.log(props);
 return (
 
   <div class = "ui card">
-    <div class = "content"> {props.children} 
+    <div class = "content">{props.children}
   
       <div class = "extracontent">
         <div class = "ui two buttons">
 
-           <div class = "first button">Approved</div>
-            <div class = "second button">Rejected</div>
+           <div class = "ui basic green button">Approved</div>
+            <div class = "ui basic red button">Rejected</div>
         </div>
       </div>
     </div> 
--- src/index.js
+++ src/index.js
@@ -11,11 +11,28 @@ const App = () => {
     
   <div className  = "ui container comments">
  <ApprovalCard>
-    <CommentDetail  author = "sam" TimeAgo ="Today 4.45pm" Comments = "Nice Blog" avatar = {faker.image.avatar()}/>  
+   <div>
+     <h4>Warning!</h4>
+     Are you sure you want to do this..
+   </div>
+   </ApprovalCard> 
+    
+   <ApprovalCard> 
+    <CommentDetail  author = "sam" TimeAgo ="Today 4.45pm" Comments = "Nice Blog" avatar = {faker.image.avatar()}/> 
+    </ApprovalCard> 
+
+
+    <ApprovalCard>
+    <CommentDetail  author = "sam" TimeAgo ="Today 4.45pm" Comments = "Nice Blog" avatar = {faker.image.avatar()}/> 
+    </ApprovalCard> 
+
+    <ApprovalCard>
     <CommentDetail  author = "sam" TimeAgo ="Today 4.45pm" Comments = "Nice Blog" avatar = {faker.image.avatar()}/>
+    </ApprovalCard>
+
+    <ApprovalCard>
     <CommentDetail author = "sam" TimeAgo ="Today 4.45pm" Comments = "Nice Blog post" avatar = {faker.image.avatar()}/>
-       
-           </ApprovalCard>
+     </ApprovalCard>
            </div>
   );
   };
